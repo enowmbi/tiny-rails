be709c6579fcf3b6eeb5f49d7c010cfc98134bd5
Enow Mbi <benowmbi@yahoo.com>
2022-05-09 05:56:02 +0100

    Initial setup of tiny-rails gem

---
+++ spec/tiny/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tiny::Rails do
+  it "has a version number" do
+    expect(Tiny::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
