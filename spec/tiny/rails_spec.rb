4ffa583feee29f06e7e66b9bd1eef0001e493608
Enow Mbi <benowmbi@yahoo.com>
2022-05-09 08:13:38 +0100

    Disable dummy examples in rails_spec.rb

--- spec/tiny/rails_spec.rb
+++ spec/tiny/rails_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Tiny::Rails do
     expect(Tiny::Rails::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  xit "does something useful" do
     expect(false).to eq(true)
   end
 end
